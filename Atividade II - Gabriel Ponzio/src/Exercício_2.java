ba2279c68dace98f26ba90b27c534a758ea69a21
Gabriel Rozanczyk Ponzio <gabriel.ponzio@acad.espm.br>
2022-11-14 23:24:40 -0300

    Upload das tarefas

---
+++ Atividade II - Gabriel Ponzio/src/Exercício_2.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Exercício_2 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		int[]numero = new int[10];
+		int valor;
+	
+		System.out.println("Gerador e Ordenador de números");
+		preencher(numero);
+		imprimir(numero);
+		ordenar(numero);
+		System.out.println("\n Número Ordenado");
+		imprimir(numero);
+		System.out.print("\nDigite um valor para busca: ");
+		valor = teclado.nextInt();
+		checagem(numero, valor);
+	}
+
+	public static void preencher(int[]numero) {
+		Random gerador = new Random();
+		int checador;
+		
+		for(int i = 0; i < numero.length; i++){
+            checador = gerador.nextInt(0,101);
+            for(int j = 0; j < numero.length; j++){
+                  if(checador == numero[j] && j != i){
+                        checador = gerador.nextInt(0,101);
+                  }else{
+                       numero[i] = checador;
+                  }
+            }
+       }
+		
+	}
+	
+	public static void imprimir(int[]numero) {
+		
+		for(int i = 0; i < numero.length; i++) {
+			System.out.print(numero[i] + "\t");
+		}
+	}
+	
+	public static void ordenar(int[]numero) {
+		  for (int i = 0; i < numero.length - 1; i++)
+		    for (int j = 0; j < numero.length - i - 1; j++)
+		      if (numero[j] > numero[j + 1]) {
+		        int aux = numero[j];
+		        numero[j] = numero[j + 1];
+		        numero[j + 1] = aux;
+		      }
+		
+	}
+	
+	public static void checagem(int[]numero, int valor) {
+		int aux, indice;
+		boolean teste = false;
+		indice = (numero.length/2) - 1;
+		aux = (numero[indice]);
+		if(valor == aux) {
+			teste = true;
+			System.out.println(teste + ", o número está na " + (indice + 1) + "º posição");
+		}else {
+			if(valor > aux){
+				for(int i = (indice+1); i < numero.length; i++) {
+					if(numero[i] == valor) {
+						teste = true;
+						System.out.println(teste + ", o número está na " + (i + 1) + "º posição");
+						return;
+					}else {
+						teste = false;
+					}
+				}
+					if(teste == false) {
+						System.out.println(teste + ", o número não se encontra na lista");
+					}
+					
+			}else if(valor < aux){
+				for(int i = (indice-1); i > -1; i--) {
+					if(numero[i] == valor) {
+						teste = true;
+						System.out.println(teste + ", o número está na " + (i + 1) + "º posição");
+					}else {
+						teste = false;
+					}
+				}
+				if(teste == false) {
+					System.out.println(teste + ", o número não se encontra na lista");
+				}
+			}
+		}
+	}
+
+}
